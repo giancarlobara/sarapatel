308288f41408ffe75d3f415443cdf127816c0132
Luca Benetti <lucabenetti@discente.ufg.br>
2024-04-17 17:34:22 -0300

    Add produtor consumidor buffer limitado c#

---
+++ ProdutorConsumidorBufferLimitadoC#/Consumidor.cs
@@ -0,0 +1,46 @@
+﻿namespace BoundedBuffer
+{
+    public class Consumidor
+    {
+        private List<int?> _buffer;
+        private readonly Semaphore _semaphore;
+
+        public Consumidor(List<int?> buffer, Semaphore semaphore)
+        {
+            _buffer = buffer;
+            _semaphore = semaphore;
+        }
+
+        public async Task Consumir(string nome)
+        {
+            await Task.Delay(1);
+
+            int? dado = 0;
+
+            try
+            {
+                _semaphore.WaitOne();
+
+                    // Não consumir quando está vazio
+                    if (_buffer.Count == 0)
+                    {
+                        Console.WriteLine($"Consumidor encontrou buffer vazio {DateTime.Now.Ticks}");
+                        return;
+                    }
+                
+                    dado = _buffer[_buffer.Count - 1];
+
+                    _buffer.RemoveAt(_buffer.Count - 1);
+                    Console.WriteLine($"Consumidor {nome} consumiu dado {dado} {DateTime.Now.Ticks}");
+            }
+
+            catch( Exception ex )
+            {
+                Console.WriteLine($"Consumidor {nome} exceção ao tentar remover dado {dado}");
+            }
+            finally{
+                _semaphore.Release();
+            }
+        }
+    }
+}
---
+++ ProdutorConsumidorBufferLimitadoC#/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace BoundedBuffer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var capacidade = 10;
+            var buffer = new List<int?>();
+
+            var semaphore = new Semaphore(1, 1);
+
+            var produtor = new Produtor(buffer, capacidade, semaphore);
+            var consumidor = new Consumidor(buffer, semaphore);
+
+            await Task.WhenAll(
+                produtor.Inserir(1, "A"),
+                produtor.Inserir(2, "B"),
+                produtor.Inserir(3, "C"),
+                consumidor.Consumir("D"),
+                consumidor.Consumir("E"),
+                consumidor.Consumir("F"));
+        }
+    }
+}
